563738d94dd27715f1961bbf000b47ffae0230fb
Siha Park <siha@Sihas-MacBook-Air.local>
2023-03-30 02:45:33 +0900

    create page and routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pageP/Profile";
+import SignIn from "./pagesSignIne";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
     <>
-      <h1 className="text-4xl">Hello World!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
